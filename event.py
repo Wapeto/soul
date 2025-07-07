42e076a8e233f51b35f81f90b32a45869db37bbd
Wapeto <54235524+Wapeto@users.noreply.github.com>
2025-07-07 22:26:48 +0200

    Add task management to Person and event

--- event.py
+++ event.py
@@ -2,7 +2,7 @@ import random
 from world import World
 from person import Person
 
-events = ['eat', 'read', 'nap']
+events = ['eat', 'read', 'nap', 'perform_task']
 
 def choose_random_event(p:Person):
     if p.event_cooldown != 0 :
@@ -19,8 +19,15 @@ def choose_random_event(p:Person):
             p.read()
         case 'nap':
             p.nap()
+        case 'perform_task':
+            if p.tasks:
+                idx = random.randrange(len(p.tasks))
+                p.complete_task(idx)
+            else:
+                p.add_task("General task")
     return event
 
 def update_event_cooldowns(world:World):
     for p in world.people:
         p.event_cooldown = max(p.event_cooldown - 1, 0)
+
